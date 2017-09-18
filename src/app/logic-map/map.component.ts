a50de3ac01b53d6be578cc55b98fc2c2b060aa38
hoakusa <hoakusa@gmail.com>
2017-09-19 02:07:36 +0300

    wire connection

--- src/app/logic-map/map.component.ts
+++ src/app/logic-map/map.component.ts
@@ -56,7 +56,58 @@ export class MapComponent implements OnInit {
 
   ngOnInit(): void {
     jsPlumb.ready(function() {
-      jsPlumb.draggable("1", { grid:[10,10] });     
+      jsPlumb.setContainer('diagramContainer'); 
+
+      // Connection
+      var input = {
+        isSource: false,
+        isTarget: true,
+        connector: ['Bezier'],
+        endpoint: 'Dot',
+        paintStyle:{ fill: '#16a085' },
+        hoverPaintStyle:{ fill: '#2ecc71' },
+        connectorStyle:{ stroke: '#34495e', strokeWidth: 5 },
+        connectorHoverStyle:{ stroke: '#2ecc71', strokeWidth: 5 }
+      };
+
+      var output = {
+        isSource: true,
+        isTarget: false,
+        connector: ['Straight'],
+        endpoint: 'Dot',
+        paintStyle:{ stroke: '#16a085', strokeWidth: 3 },
+        hoverPaintStyle:{ fill: '#2ecc71' },
+        connectorStyle:{ stroke: '#34495e', strokeWidth: 5 },
+        connectorHoverStyle:{ stroke: '#2ecc71', strokeWidth: 5 }
+      };
+
+      jsPlumb.addEndpoint('0', { 
+        anchors: [0, 0.66, 1, 0] // LeftIn
+      }, input);
+
+      jsPlumb.addEndpoint('0', { 
+        anchors: [0, 0.33, 1, 0] // LeftIn
+      }, input);
+
+      jsPlumb.addEndpoint('0', { 
+        anchors: [1, 0.5, 1, 0] // RightOut
+      }, output);
+
+      jsPlumb.draggable('0', { grid:[5,5] });
+
+      jsPlumb.addEndpoint('1', { 
+        anchors: [0, 0.66, 1, 0] // LeftIn
+      }, input);
+
+      jsPlumb.addEndpoint('1', { 
+        anchors: [0, 0.33, 1, 0] // LeftIn
+      }, input);
+
+      jsPlumb.addEndpoint('1', { 
+        anchors: [1, 0.5, 1, 0] // RightOut
+      }, output);
+
+      jsPlumb.draggable('1', { grid: [5,5] });
     }); 
   }
 
